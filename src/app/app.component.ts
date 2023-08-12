0f84f2c97d3c810ae926634b700fef4ed79a08e4
Lucas Rodrigues <lucas.android106@gmail.com>
2023-08-12 18:09:01 -0300

    add site completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+interface IApps {
+  name: string,
+  img: string,
+  googlePlayLink: string,
+  appStoreLink: string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myApp';
+  title = 'my-angular-project-app';
+  img: string = "../../assets/imgs/logoCell.webp";
+
+  apps: IApps[] = []
+
+  constructor() {
+    this.getAppsFromAJsonFile();
+  }
+
+  // async ngOnInit() {
+  //   await this.getAppsFromAJsonFile();
+  // }
+
+  async getAppsFromAJsonFile() {
+    await (await fetch("../../assets/allApps.json")).json().then((result) => {
+      this.apps = result;
+    });
+  }
+
+  openLink(link: string) {
+    window.open(link);
+
+  }
 }
